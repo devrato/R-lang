eb56d2fca60f0b1ea417a59734031cf26e3f0a0e
devrato <devratoroy001@gmail.com>
2021-07-01 20:11:07 +0530

    Sesh 4

---
+++ day2.R
@@ -0,0 +1,42 @@
+#Salary
+
+#Games
+
+#MinutesPlayed
+?list
+lst <- list("A", "B", c(5:10), TRUE, 51.23, 119.1)
+
+lst[[3]]
+
+#matrices
+?matrix
+
+data <- 1:20
+
+data
+
+A <- matrix(data, 5, 5)
+
+rownames(A) <- list('A','B','C','D','E')
+
+A
+
+B <- matrix(1:12, nrow = 3, dimnames = list(c(2001, 2002, 2003), month.abb[4:7]))
+
+?rbind
+rownames(B) <- (month.abb[1:5])
+
+head(,3)
+
+tail(B, 3)
+
+rownames(B)
+
+B
+
+B[1,2]
+
+A[1,]
+
+
+data
